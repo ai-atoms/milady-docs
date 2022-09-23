dcf595aac1e2ce75fcdb38c6bb8cca9e9224f310
Arnaud Allera <45487966+arn-all@users.noreply.github.com>
2022-09-23 10:47:19 +0200

    Update conf.py

--- src/conf.py
+++ src/conf.py
@@ -27,8 +27,11 @@ author = 'MCM, AMG & more'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['sphinxcontrib.email'
 ]
+
+# Obfuscate email addresses
+email_automode = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['templates']
